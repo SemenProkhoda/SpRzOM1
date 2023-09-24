c0b935b1c317035de5beeae0d07a9359a0e89a91
SemenProkhoda <semen.prokhoda@siemens-healthineers.com>
2023-09-24 19:23:33 +0200

    24.09.2023

--- SpRzOM1/Large.hpp
+++ SpRzOM1/Large.hpp
@@ -12,11 +12,26 @@ public:
     Large();
     Large(const string& str);
     Large(uint64_t value);
+
     void fromString(const string& str);
     void fromUInt64(uint64_t value);
     string toString() const;
+
     Large operator+(const Large& other) const;
+    Large operator-(const Large& other) const;
     Large operator*(const Large& other) const;
+    Large operator/(const Large& other) const;
+    Large operator%(const Large& other) const;
+    Large operator^(uint64_t exp) const;
+    bool operator==(const Large& other) const;
+    bool operator!=(const Large& other) const;
+    bool operator<= (const Large & other) const;
+    bool operator>=(const Large& other) const;
+    bool operator>(const Large& other) const;
+    bool operator<(const Large& other) const;
+    int highestSetBit() const;
+    Large operator<<(int shift) const;
+    Large operator>>(int shift) const;
 
 };
 
@@ -110,6 +125,41 @@ inline Large Large::operator+(const Large& other) const {
     return result;
 }
 
+inline Large Large::operator-(const Large& other) const
+{
+    Large result;
+    int carry = 0;
+
+    size_t max_size = max(data.size(), other.data.size());
+    result.data.resize(max_size);
+
+    for (size_t i = 0; i < max_size; i++) {
+        int sub = carry;
+        if (i < data.size()) {
+            sub += data[i];
+        }
+        if (i < other.data.size()) {
+            sub -= other.data[i];
+        }
+
+        if (sub < 0) {
+            sub += BASE;
+            carry = -1;
+        }
+        else {
+            carry = 0;
+        }
+
+        result.data[i] = sub;
+    }
+
+    while (result.data.size() > 1 && result.data.back() == 0) {
+        result.data.pop_back();
+    }
+
+    return result;
+}
+
 inline Large Large::operator*(const Large& other) const {
     Large result;
     result.data.resize(data.size() + other.data.size());
@@ -121,6 +171,181 @@ inline Large Large::operator*(const Large& other) const {
             result.data[i + j] = product % BASE;
             carry = product / BASE;
         }
+    }
+
+    while (result.data.size() > 1 && result.data.back() == 0) {
+        result.data.pop_back();
+    }
+
+    return result;
+}
+
+inline Large Large::operator/(const Large& other) const
+{
+    if (other == 0) {
+        throw std::runtime_error("Division by zero.");
+    }
+
+    Large quotient;
+    Large remainder = *this;
+    Large divisor = other;
+    Large factor;
+
+    while (remainder >= divisor) {
+        factor = divisor;
+        Large temp(1);
+
+        while (remainder >= factor) {
+            remainder = remainder - factor;
+            quotient = quotient + temp;
+            factor = factor << 1;
+            temp = temp << 1;
+        }
+    }
+
+    return quotient;
+}
+
+inline Large Large::operator%(const Large& other) const
+{
+    Large quotient;
+    Large remainder = *this;
+    Large divisor = other;
+
+    if (other == 0) {
+        throw runtime_error("Division by zero.");
+    }
+
+    while (remainder >= divisor) {
+        int shift = remainder.highestSetBit() - divisor.highestSetBit();
+        Large temp = divisor << shift;
+        while (remainder >= temp) {
+            remainder = remainder - temp;
+            quotient = quotient + (Large(1) << shift);
+        }
+    }
+
+    return remainder;
+}
+
+inline Large Large::operator^(uint64_t exp) const
+{
+    if (exp == 0) {
+        return Large(1);
+    }
+
+    Large result = *this;
+    Large base = *this;
+    exp--;
+
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = result * base;
+        }
+        base = base * base;
+        exp /= 2;
+    }
+
+    return result;
+}
+
+inline bool Large::operator==(const Large& other) const
+{
+    return data == other.data;
+}
+
+inline bool Large::operator!=(const Large& other) const
+{
+    return data != other.data;
+}
+
+inline bool Large::operator<=(const Large& other) const
+{
+    return data <= other.data;
+}
+
+inline bool Large::operator>=(const Large& other) const
+{
+    return data >= other.data;
+}
+
+inline bool Large::operator>(const Large& other) const
+{
+    return data > other.data;
+}
+
+inline bool Large::operator<(const Large& other) const
+{
+    return data < other.data;
+}
+
+inline int Large::highestSetBit() const
+{
+    for (int i = data.size() - 1; i >= 0; i--) {
+        if (data[i] != 0) {
+            for (int j = 31; j >= 0; j--) {
+                if ((data[i] >> j) & 1) {
+                    return static_cast<int>(i * 32 + j);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+inline Large Large::operator<<(int shift) const
+{
+    if (shift < 0) {
+        throw invalid_argument("Negative shift value.");
+    }
+
+    Large result;
+    int shiftDigits = shift / 9;
+    int shiftBits = shift % 9;
+
+    result.data.resize(data.size() + shiftDigits + 1, 0);
+
+    for (size_t i = 0; i < data.size(); i++) {
+        uint64_t bits = data[i];
+        uint64_t lowBits = bits << shiftBits;
+        uint64_t carry = lowBits / BASE;
+        result.data[i + shiftDigits] += lowBits % BASE;
+
+        for (int j = 1; carry > 0; j++) {
+            result.data[i + shiftDigits + j] += carry;
+            carry = result.data[i + shiftDigits + j] / BASE;
+            result.data[i + shiftDigits + j] %= BASE;
+        }
+    }
+
+    while (result.data.size() > 1 && result.data.back() == 0) {
+        result.data.pop_back();
+    }
+
+    return result;
+}
+
+inline Large Large::operator>>(int shift) const
+{
+    if (shift < 0) {
+        throw invalid_argument("Negative shift value.");
+    }
+
+    Large result;
+    int shiftDigits = shift / 9;
+    int shiftBits = shift % 9;
+
+    if (shiftDigits >= data.size()) {
+        return Large(0);
+    }
+
+    result.data.resize(data.size() - shiftDigits, 0);
+
+    for (size_t i = data.size() - 1; i >= shiftDigits; i--) {
+        uint64_t bits = data[i];
+        uint64_t highBits = bits >> shiftBits;
+        uint64_t lowBits = (i > 0) ? (data[i - 1] << (9 - shiftBits)) : 0;
+        result.data[i - shiftDigits] = highBits + (lowBits % BASE);
     }
 
     while (result.data.size() > 1 && result.data.back() == 0) {
--- SpRzOM1/SpRzOM1.cpp
+++ SpRzOM1/SpRzOM1.cpp
@@ -5,14 +5,34 @@
 using namespace std;
 
 int main() {
-    Large num1("9823748905723984750982347597812340875442");
-    Large num2("7189236597231846102983461283649872630212");
+    Large num1("9121234541");
+    Large num2("2123234541");
 
     Large sum = num1 + num2;
     cout << "Sum: " << sum.toString() << endl;
 
-    Large product = num1 * num2;
-    cout << "Product: " << product.toString() << endl;
+    Large difference = num1 - num2;
+    cout << "Difference: " << difference.toString() << "\n";
 
+    Large product = num1 * num2;
+    cout << "Product: " << product.toString() << "\n";
+
+    Large quotient = num1 / num2;
+    cout << "Quotient: " << quotient.toString() << "\n";
+
+    Large remainder = num1 % num2;
+    cout << "Remainder: " << remainder.toString() << "\n";
+
+    Large power = num1 ^ 5;
+    cout << "Power: " << power.toString() << "\n";
+
+    int highestBit = num1.highestSetBit();
+    cout << "Highest Set Bit: " << highestBit << "\n";
+
+    Large leftShift = num1 << 3;
+    cout << "Left Shift: " << leftShift.toString() << "\n";
+
+    Large rightShift = num1 >> 3;
+    cout << "Right Shift: " << rightShift.toString() << "\n";
     return 0;
 }
